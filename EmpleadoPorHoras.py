2ec96b93a342028c8095bd79854c1789fcaadef4
dt <dtpollo04@gmail.com>
2025-11-06 13:00:20 -0500

    Documentacion extra del codigo

--- EmpleadoPorHoras.py
+++ EmpleadoPorHoras.py
@@ -1,10 +1,12 @@
 from Empleados import Empleado
 
 class EmpleadoPorHoras(Empleado):
+    # Inicializa al EmpleadoPorHoras
     def __init__(self, nombre, apellido, id, salario_base, horas_trabajadas, pago_por_hora):
         super().__init__(nombre, apellido, id, salario_base)
         self._horas_trabajadas = horas_trabajadas
         self._pago_por_hora = pago_por_hora
 
+    # Calcula el salario total (Polimorfismo)
     def calcularSalario(self):
         return self._horas_trabajadas * self._pago_por_hora
--- EmpleadoTiempoCompleto.py
+++ EmpleadoTiempoCompleto.py
@@ -1,5 +1,6 @@
 from Empleados import Empleado
 
 class EmpleadoTiempoCompleto(Empleado):
+    # Calcula el salario base (Polimorfismo)
     def calcularSalario(self):
         return self._salario_base + self._salario_base * 0.10
